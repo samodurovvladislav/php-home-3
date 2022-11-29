07c57a5948e59c56a6fdd6712517a76f274d35bd
Vladislav Samodurov <26804585+samodurovvladislav@users.noreply.github.com>
2022-11-29 13:50:21 +0200

    Homework № 4

---
+++ Color.php
@@ -0,0 +1,112 @@
+<?php 
+
+namespace php_latest;
+
+use InvalidArgumentException;
+
+class Color{
+
+    private int $red;
+    private int $green;
+    private int $blue;
+
+    public function __construct(int $red, int $green, int $blue)
+    {
+
+        $this->setRed($red);
+        $this->setGreen($green);
+        $this->setBlue($blue);
+
+    }
+
+    public function getRed(): int
+    {
+        return $this->red;
+    }
+
+    public function getGreen(): int
+    {
+        return $this->green;
+    }
+
+    public function getBlue(): int
+    {
+        return $this->blue;
+    }
+    
+    public function mix(Color $ColorObject): Color
+    {
+
+        $red = ($this->getRed() + $ColorObject->getRed())/2;
+
+        $green = ($this->getGreen() + $ColorObject->getGreen())/2;
+
+        $blue = ($this->getBlue() + $ColorObject->getBlue())/2;
+
+        return new Color($red, $green, $blue);
+
+    }
+
+    public function equals(Color $Color): bool
+    {
+
+        if ($this !== $Color){
+
+            return false;
+
+        }
+
+        return true;
+
+    }
+
+    protected function setRed(int $color): int
+    {
+
+        $this->checkColor($color);
+
+        return $this->red = $color;
+
+    }
+
+    protected function setGreen(int $color): int
+    {
+
+        $this->checkColor($color);
+
+        return $this->green = $color;
+
+    }
+
+    protected function setBlue(int $color): int
+    {
+
+        $this->checkColor($color);
+
+        return $this->blue = $color;
+
+    }
+
+    protected function checkColor(int $value): bool|InvalidArgumentException
+    {
+
+        if ($value < 0 || $value > 255){
+            throw new InvalidArgumentException("Некоректне значення кольору!", 422);
+        }
+
+        return true;
+
+    }    
+
+    public static function random(): Color
+    {
+
+        $random_red = rand(0, 255);
+        $random_blue = rand(0, 255);
+        $random_green = rand(0, 255);
+
+        return new Color($random_red, $random_green, $random_blue);
+
+    }
+
+}
--- index.php
+++ index.php
@@ -1,5 +1,10 @@
 <?php 
 
+namespace php_latest;
+
+use PDO;
+use PDOException;
+
 $host= 'db';
 $db = 'php-latest';
 $user = 'postgres';
@@ -7,11 +12,45 @@ $password = 'postgres';
 
 $dsn = "pgsql:host=$host;port=5432;dbname=$db;";
 
+require __DIR__ . "/vendor/autoload.php";
+
 try{
     $connection = new PDO($dsn, $user, $password, [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
 } catch (PDOException $exc){
-    echo "<p style='color:red'> {$exc->getMessage()} </p>";
+    echo "<p style='color:red;'> {$exc->getMessage()} </p>";
     exit(1);
 }
 
-var_dump($connection);
+$Color = new Color(200, 200, 200);
+
+echo "<h2>Створення класу кольору:</h2>";
+
+echo "червоний: " . $Color->getRed() . "<br>";
+echo "зелений: " . $Color->getGreen() . "<br>";
+echo "синій: " . $Color->getBlue() . "<br>";
+
+$Color2 = clone $Color;
+
+echo "<h2>Порівняння двох класів:</h2>";
+
+if ($Color->equals($Color2)){
+    echo "<p>Класи співпадають</p><br>";
+} else {
+    echo "<p>Класи не співпадають</p><br>";
+}
+
+echo "<br>";
+
+$Color3 = new Color(100, 100, 100);
+
+$mixedColor = $Color->mix($Color3);
+
+echo "<h2>Змішування кольорів:</h2>";
+
+echo "Отриманий червоний: " . $mixedColor->getRed() . "<br>";
+echo "Отриманий зелений: " . $mixedColor->getGreen() . "<br>";
+echo "Отриманий синій: " . $mixedColor->getBlue() . "<br>";
+
+echo "<br>";
+
+?>
